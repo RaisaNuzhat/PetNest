284bc4e16408f251821aee728ad2d34f279a69e0
RaisaNuzhat <raisa.csecu@gmail.com>
2024-06-22 13:20:00 +0600

    all user page done

--- src/Pages/Dashboard/Admin/AllUsers.jsx
+++ src/Pages/Dashboard/Admin/AllUsers.jsx
@@ -1,9 +1,70 @@
-
-
+import {  useEffect, useState } from "react";
+import { Helmet } from "react-helmet";
+import axios from "axios";
+import UserTable from "../../../components/UserTable";
 const AllUsers = () => {
+    const [items, setItem] = useState([])
+    console.log(items)
+    useEffect(() => {
+        const getData =  async() =>
+            {
+                const {data} = await axios(`${import.meta.env.VITE_API_URL}/users`)
+                setItem(data)
+            }
+        getData()
+    }, [])
     return (
         <div>
-            <h3>hi</h3>
+             <div className='container mx-auto px-4 sm:px-8'>
+        <Helmet>
+          <title>All Users</title>
+        </Helmet>
+        <div className='py-8'>
+          <div className='-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto'>
+            <div className='inline-block min-w-full shadow rounded-lg overflow-hidden'>
+              <table className='min-w-full leading-normal'>
+                <thead>
+                  <tr>
+                    <th
+                      scope='col'
+                      className='px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal'
+                    >
+                      Email
+                    </th>
+                    <th
+                      scope='col'
+                      className='px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal'
+                    >
+                      Role
+                    </th>
+                    <th
+                      scope='col'
+                      className='px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal'
+                    >
+                      Status
+                    </th>
+
+                    <th
+                      scope='col'
+                      className='px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal'
+                    >
+                      Action
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                {
+                items.map(item => <UserTable
+                 key={item._id}
+                item ={item}
+                >
+                </UserTable>)}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
         </div>
     );
 };
---
+++ src/components/UserTable.jsx
@@ -0,0 +1,43 @@
+/* eslint-disable react/prop-types */
+
+const UserTable = ({ item }) => {
+   console.log(item)
+  return (
+    <tr>
+      <td className='px-5 py-5 border-b border-gray-200 bg-white text-sm'>
+        <p className='text-gray-900 whitespace-no-wrap'>{item?.email}</p>
+      </td>
+      <td className='px-5 py-5 border-b border-gray-200 bg-white text-sm'>
+        <p className='text-gray-900 whitespace-no-wrap'>{item?.role}</p>
+      </td>
+       <td className='px-5 py-5 border-b border-gray-200 bg-white text-sm'>
+        {item?.status ? (
+          <p
+            className={`${
+              item.status === 'Verified' ? 'text-green-500' : 'text-yellow-500'
+            } whitespace-no-wrap`}
+          >
+            {item.status}
+          </p>
+        ) : (
+          <p className='text-red-500 whitespace-no-wrap'>Unavailable</p>
+        )}
+      </td> 
+
+      <td className='px-5 py-5 border-b border-gray-200 bg-white text-sm'>
+        <span className='relative cursor-pointer inline-block px-3 py-1 font-semibold text-green-900 leading-tight'>
+          <span
+            aria-hidden='true'
+            className='absolute inset-0 bg-green-200 opacity-50 rounded-full'
+          ></span>
+          <span className='relative'>Update Role</span>
+        </span>
+        {/* Update User Modal */}
+      </td>
+    </tr>
+  )
+}
+
+
+
+export default UserTable
